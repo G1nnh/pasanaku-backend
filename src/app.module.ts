3638ff67ce5c33f9fa6d2d68b4d158b7f7a9e41c
joseluis <joseluis.universidad2020@gmail.com>
2024-03-19 18:29:07 -0400

    Se creó el CRUD para ROLES

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [PrismaModule, UsuariosModule],
+  imports: [PrismaModule, UsuariosModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
